29cb53bf840aba0eb08b79a070bed629c748cdcb
Ries van Geffen <387211@student.fontys.nl>
2020-11-18 10:49:28 +0100

    Removed AbstractStrijpProtocol because it has no usage

--- agent/src/main/java/org/openremote/agent/protocol/strijp/StrijpProtocol.java
+++ agent/src/main/java/org/openremote/agent/protocol/strijp/StrijpProtocol.java
@@ -15,7 +15,6 @@ import org.openremote.model.value.Values;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
